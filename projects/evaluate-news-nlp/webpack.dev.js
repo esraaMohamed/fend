7e176b5436db7902d8aff18b8330d682068076a3
Esraa Afifi <esraaafifi@Esraas-MacBook-Pro.local>
2020-09-10 11:39:32 -0700

    Adding webpack loaders and plugins, configuring them for both prod and dev environments

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -2,18 +2,25 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                uses: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
